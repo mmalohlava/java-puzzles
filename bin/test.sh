11e5cd3cec373da8dee723cdfd0c3a7377b9fb5c
mmalohlava <michal.malohlava@gmail.com>
2012-12-12 20:27:11 +0100

    test for tests

---
+++ bin/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# test.sh -- created 2012-12-12, <+NAME+>
+# @Last Change: 24-Dez-2004.
+# @Revision:    0.0
+
+for i in $(seq 1 1); do
+    echo "Step $i..."
+    sleep 1
+done
+
+exit -1
+
+# vi: 
